422cbfb65853590002d5faffe510e35f779ea13a
Rodrigo Eduardo Carcuz Ortega <53447333+carcuz789@users.noreply.github.com>
2020-06-06 05:35:10 -0600

    06/06/2020

--- src/practica1_201700633/Lexico.java
+++ src/practica1_201700633/Lexico.java
@@ -10,13 +10,13 @@ import java.util.ArrayList;
 /**
  *
  * @author carcu
- * terminado comentario uniliea solo 
+ * terminado comentario uniliea M LETRAS IDS, NUMEROS
  */
 public class Lexico {
        public static ArrayList<String> tipo=null;
        public static ArrayList<String> tokens=null;
        public static ArrayList<String> error=null;
-    public static ArrayList<String> lex1(String texto){
+    public static void lex1(String texto){
         int con=0;
         String conca="";
         
@@ -26,12 +26,52 @@ public class Lexico {
             if (Character.isLetter(letras[con])) {  // ids o letras
                  conca+=letras[con];
                  con++;
+                 boolean var = false;
+                 boolean estadin = true;
                  do {
                     
-                } while (true);
+                     if (letras[con]=='_') {
+                         var= true;
+                          do {
+                             conca+=letras[con];
+                             con++;                             
+                         } while (letras[con]=='_');
+                         
+                     }else if (Character.isDigit( letras[con])) {
+                         do {
+                             var = true;
+                             conca+=letras[con];
+                             con++;
+                         } while (Character.isDigit( letras[con]));                         
+                     }else if (Character.isLetter(letras[con])) {
+                         do {
+                             conca+=letras[con];
+                             con++;
+                         } while (Character.isLetter(letras[con]));                         
+                     }else{
+                         estadin =false;
+                     }
+                } while (estadin==true);
+                 //si es una texto se va a un lugar y si es id a otro
+                 if (var==false) {
+                    tokens.add(conca);
+                    tipo.add("TEXTO");
+                    conca="";
+                }else{
+                    tokens.add(conca);
+                    tipo.add("TEXTO ID");
+                    conca="";
+                 }
                 
             }
             else if (Character.isDigit(letras[con])) {  // numeros
+                do {
+                     conca+=letras[con];
+                     con++;
+                } while (Character.isDigit(letras[con]));
+                 tokens.add(conca);
+                 tipo.add("NUMERO");
+                 conca="";
                 
             }
             else if (letras[con]=='\\') { // comentarios de linea
@@ -61,16 +101,34 @@ public class Lexico {
                     do {
                         conca+=letras[con];
                         con++;
+                        if (letras[con]=='!') {
+                            conca+=letras[con];
+                            con++;
+                            if (letras[con]=='>') {
+                                 conca+=letras[con];
+                                 con++;
+                                 estado =false;
+                            }
+                        }
                     } while (estado==true);
+                    tokens.add(conca);
+                    tipo.add("COMENTARIO MULTILINEA");
+                    conca="";
                     
                 }else{
-                    error(conca);
-                }
-                
+                    error(conca);//ESTE ERROR ES POR SI VIENE < PERO NO VIENE DESPUES !
+                }                
+            }else if(letras[con]=='-'){
+                conca+=letras[con];
+                con++;
+                tokens.add(conca);
+                tipo.add("GUION");
+                //el guion es correcto
+            }else if(Character.isWhitespace(letras[con])){
+                con++; // si es espacion en blanco 
             }
         } while (con<letras.length);
         
-        return tokens;
                 
     }
     public static void error(String Lex){
