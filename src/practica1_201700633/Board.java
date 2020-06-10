fb5e41f159cdd2f7d75a209c708f2846070b8629
carcuz789 <carcuz789@gmail.com>
2020-06-10 13:33:16 -0600

    09/jun/2020 rod 1:33

--- src/practica1_201700633/Board.java
+++ src/practica1_201700633/Board.java
@@ -107,7 +107,7 @@ public class Board extends JPanel implements ActionListener {
             for (int j = 0; j < BoardWidth; ++j) {
                 Tetrominoes shape = shapeAt(j, BoardHeight - i - 1);
                 if (shape != Tetrominoes.NoShape)
-                    drawSquare(g, 0 + j * squareWidth(),
+                    dibuja(g, 0 + j * squareWidth(),
                                boardTop + i * squareHeight(), shape);
             }
         }
@@ -116,7 +116,7 @@ public class Board extends JPanel implements ActionListener {
             for (int i = 0; i < 4; ++i) {
                 int x = curX + curPiece.x(i);
                 int y = curY - curPiece.y(i);
-                drawSquare(g, 0 + x * squareWidth(),
+                dibuja(g, 0 + x * squareWidth(),
                            boardTop + (BoardHeight - y - 1) * squareHeight(),
                            curPiece.getShape());
             }
@@ -225,15 +225,15 @@ public class Board extends JPanel implements ActionListener {
         }
      }
 
-    private void drawSquare(Graphics g, int x, int y, Tetrominoes shape)
+    private void dibuja(Graphics g, int x, int y, Tetrominoes shape)
     {
         Color colors[] = { new Color(0, 0, 0), new Color(204, 102, 102), 
-            new Color(102, 204, 102), new Color(102, 102, 204), 
-            new Color(204, 204, 102), new Color(204, 102, 204), 
-            new Color(102, 204, 204), new Color(218, 170, 0)
+            new Color(102, 204, 102),new Color(102, 204, 204), 
+             new Color(204, 102, 204),new Color(204, 204, 102), 
+            new Color(218, 170, 0), new Color(102, 102, 204)
         };
 
-
+ 
         Color color = colors[shape.ordinal()];
 
         g.setColor(color);
